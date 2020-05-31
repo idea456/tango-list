f4ebbbe92bf966f5974f72a92468a15080ec3d3d
Adrienne Rio Wongso Atmojo <riowongsoatmojo@gmail.com>
2020-05-31 19:07:14 +0800

    finalizing project

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-app-bar app>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-    <v-toolbar-title>📝🇯🇵 単語リスト</v-toolbar-title>
-  </v-app-bar>
+      <v-toolbar-title>📝🇯🇵 単語リスト</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-subheader>📝🇯🇵 単語リスト</v-subheader>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item @click>
+          <v-list-item-content>
+            <v-list-item-title
+              ><a
+                style="text-decoration: none; color: black; font-size: 16px"
+                href="https://github.com/idea456/tango-list"
+              >
+                View in Github</a
+              ></v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "MainHeader"
+  name: "MainHeader",
+  data() {
+    return {
+      drawer: false
+    };
+  }
 };
 </script>
